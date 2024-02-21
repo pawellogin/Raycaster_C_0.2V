87acef3604cf9a22bc3ae8fe14c17bc688715640
pawellogin <pawellogin712@gmail.com>
2024-02-21 15:46:08 +0100

    Add config files

---
+++ config/mapConfig.h
@@ -0,0 +1,7 @@
+#ifndef MAPCONFIG_H
+#define MAPCONFIG_H
+
+#define MAP_WIDTH 16
+#define MAP_HEIGHT 16
+
+#endif
---
+++ config/miniMapConfig.h
@@ -0,0 +1,10 @@
+#ifndef MINIMAPCONFIG_H
+#define MINIMAPCONFIG_H
+
+#include "mapConfig.h"
+
+#define MINIMAP_SCALE 10
+#define MINIMAP_OFFSET_X (1600 - MINIMAP_SCALE * MAP_WIDTH)
+#define MINIMAP_OFFSET_Y 0 
+
+#endif
